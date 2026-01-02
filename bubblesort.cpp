8aaf89c2091fb9745c4897117122e515983d89f4
Teja jamalapurapu <tejajamalapurapu@gmail.com>
2026-01-02 09:50:11 +0530

    Add files via upload

---
+++ bubblesort.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+using namespace std;
+void bubblesort(int arr[],int n){
+       for(int i=0;i<n;i++){
+        for(int j=0;j<n-i-1;j++){
+          if(arr[j]>arr[j+1]){
+            swap(arr[j],arr[j+1]);
+          }
+        }
+       }
+}
+int main(){
+  int arr[]={12,7,6,2,1,4};
+  int n=sizeof(arr)/sizeof(arr[0]);
+  bubblesort(arr,n);
+  for(int i=0;i<n;i++){
+    cout<<arr[i]<<",";
+  }
+  return 0;
+}
